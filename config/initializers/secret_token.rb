3bb5c574f3db36ae9358658870b8788fea15bdb5
Ka Kui Cheng Ye <kakuichengye@gmail.com>
2014-04-14 01:49:46 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rails8HwFb::Application.config.secret_key_base = '3d3398b9cd8d19f9db5e11b3e0f7e16ec2798a374bbd0f500f25aa0fbb3b8822306e16d5d028e512e64e8908ac5e40d316fa7230a9d698e399e68f5d575d53ec'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Rails8HwFb::Application.config.session_store :cookie_store, key: '_rails8-hw-fb_session'
